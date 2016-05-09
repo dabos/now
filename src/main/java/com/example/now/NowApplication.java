53261b53a603d78e83b81febcdc95d22c319392f
alex.bostan <alex.bostan@hostelworld.com>
2016-05-09 13:00:04 +0100

    resynced .gitignore

---
+++ src/main/java/com/example/now/NowApplication.java
@@ -0,0 +1,12 @@
+package com.example.now;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NowApplication.class, args);
+	}
+}
